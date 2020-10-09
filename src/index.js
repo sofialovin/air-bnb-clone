3f7ed02f45736d7a9ec4b888706d53f817435aa7
Sofia Lovin-Radocea <sofia.lovin1@gmail.com>
2020-10-09 15:34:35 +0200

    practice

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Hello from './components/hello';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Hello firstName="Boris" lastName="Paillard"/>
+    <Hello firstName="Romain" lastName="ABC"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
