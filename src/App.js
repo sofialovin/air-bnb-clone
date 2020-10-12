4644dc838a3647739966c73ad90ad07df648490f
Sofia Lovin-Radocea <sofia.lovin1@gmail.com>
2020-10-12 12:48:27 +0200

    practice state

--- src/App.js
+++ src/App.js
@@ -1,34 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Flat from './components/flat';
 // import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  const flat = {
-    "name": "Cool apartment",
-    "imageUrl": "https://images.unsplash.com/photo-1582268611958-ebfd161ef9cf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
-    "price": 200,
-    "priceCurrency": "EUR",
-    "lat": 48.885707,
-    "lng": 2.343543
-  };
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      flats: []
+    };
+  }
+  componentDidMount() {
+        console.log("did mount");
+      }
 
-  return (
-    <div className="app">
-      <div className="main">
-        <div className="search">
 
-        </div>
-        <div className="flats">
-          <Flat flat={flat} />
-          <Flat flat={flat} />
-        </div>
-        <div className="map">
+  render() {
+    return (
+      <div className="app">
+        <div className="main">
+          <div className="search">
 
-        </div>
+          </div>
+          <div className="flats">
+            {this.state.flats.map((flat) => {
+              return <Flat flat={flat} />
+            })}
+
+          </div>
+         </div>
+          <div className="map">
+
+          </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
 export default App;
